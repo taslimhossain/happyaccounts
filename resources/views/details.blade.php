a0f5f37e4b948a8c8e33878f0d09c91f519fc5ac
Md.Minhajul Islam <31633249+differentcoder@users.noreply.github.com>
2023-05-16 10:52:07 +0600

    update transaction details page

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -15,7 +15,6 @@
       <div class="mt-6 border-t border-gray-100">
         <dl class="divide-y divide-gray-100">
           <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
-            
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> Date Enter User Name:  </dd>
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> Project Name :  </dd>
           </div>
@@ -26,13 +25,92 @@
             <dt class="font-bold leading-6 text-base text-gray-900">Particulars:  </dt>
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> All inifomation dynamic </dd>
           </div>
+          <!---Account to Account Money Transfer -->
+          <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
+            <dt class="font-bold leading-6 text-base text-gray-900">Account to Account Money Transfer</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account From:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account To:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Amount:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Date:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Reference:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Note:</span> </dd>
+          </div>
+            <!--- / Account to Account Money Transfer -->
+          <!--- Deposit & withdraw Transfer -->
+          <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
+            <dt class="font-bold leading-6 text-base text-gray-900"> Deposit & withdraw Transfer</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account From:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Type : </span> Deposit Or Withdraw </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Amount:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Date:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Reference:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Note:</span> </dd>
+          </div>
+            <!--- / Deposit & withdraw Transfer -->
+          <!--- Vendor Transaction -->
+          <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
+            <dt class="font-bold leading-6 text-base text-gray-900">Vendor Transaction</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account Name:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Vendor Name :</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Expenses cateogry:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Amount:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Trans Date:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Reference:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Note:</span> </dd>
+          </div>
+            <!--- / Vendor Transaction -->
+          <!--- Client Transaction -->
+          <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
+            <dt class="font-bold leading-6 text-base text-gray-900">Client Transaction</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Transaction type:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account Name:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Client Name :</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Amount:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Trans Date:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Reference:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Note:</span> </dd>
+          </div>
+            <!--- / Client Transaction -->
+          <!--- Project Other Transaction -->
+          <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
+            <dt class="font-bold leading-6 text-base text-gray-900">Project Other Transaction</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account Name:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Expenses cateogry:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Amount:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Trans Date:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Reference:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Note:</span> </dd>
+          </div>
+            <!--- / Project Other Transaction -->
+          <!--- Office Transaction -->
+          <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
+            <dt class="font-bold leading-6 text-base text-gray-900">Office Transaction</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account Name:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Expenses cateogry:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Amount:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Trans Date:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Reference:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Note:</span> </dd>
+          </div>
+            <!--- / Office Transaction -->
+          <!--- Staff Salary Transaction -->
+          <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
+            <dt class="font-bold leading-6 text-base text-gray-900">Staff Salary Transaction</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account Name:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Staff Name :</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Amount:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Trans Date:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Reference:</span> </dd>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Note:</span> </dd>
+          </div>
+            <!--- / Staff Salary Transaction -->
+        
           <div class="px-2 py-2 sm:grid sm:grid-cols-0 sm:gap-0 sm:px-0">
             <dt class="font-bold leading-6 text-base text-gray-900">Bank</dt>
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Bank name:</span> {{ $bank_transaction->bankName->bank_name }}</dd>
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Branch:</span> {{ $bank_transaction->bankName->branch }}</dd>
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account name:</span> {{ $bank_transaction->bankName->account_name }}</dd>
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> <span class="font-semibold">Account number:</span> {{ $bank_transaction->bankName->account_number }}</dd>
-
           </div>
           {{-- <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt class="text-sm font-medium leading-6 text-gray-900">Email address</dt>
